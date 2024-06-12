f0bb39ce117ca898d8aca9f2b14710120fe3ec15
sarrthak008 <sarthaknavale08@gmail.com>
2024-06-12 15:47:53 +0530

    make responsive for mobile

--- javaScript/script.js
+++ javaScript/script.js
@@ -3,9 +3,12 @@
 
 let openIcon = document.querySelector("#right img");
 let closeIcon = document.querySelector("#nav-page-cross i")
+let showDiv = document.querySelector("#show")
+let navInfoPage = document.querySelector("#nav-info-page")
  
 let navTimelien=gsap.timeline();
 
+// crate a time lien..
 
 navTimelien.to("#nav-info-page",{
     right:0,
@@ -18,7 +21,9 @@ navTimelien.from("#nav-info-page span",{
     stagger:0.3,
     color:`#7bd1ee`
 })
-navTimelien.pause();
+navTimelien.pause(); //pause the time line...
+
+// handiling time line...
 
 openIcon.addEventListener('click',()=>{
     navTimelien.play();
@@ -36,3 +41,26 @@ document.querySelector(".nav-home").addEventListener("click",()=>{
 document.querySelector(".nav-github").addEventListener("click",()=>{
     window.open("https://github.com/sarrthak008")
 })
+
+document.addEventListener("mousemove",(dets)=>{
+  showDiv.style.top=`${dets.clientY}px`
+  showDiv.style.left=`${dets.clientX}px`
+})
+
+let spns=document.querySelectorAll("#nav-info-page span")
+ for(let i =0 ; i<spns.length;i++){
+     console.log(spns[i])
+       spns[i].addEventListener("mouseenter",()=>{
+         showDiv.style.height="55px"
+         showDiv.style.width="55px"
+         showDiv.style.backgroundColor=`#7bd1ee`
+       })
+
+       spns[i].addEventListener("mouseleave",()=>{
+        showDiv.style.height="50px"
+         showDiv.style.width="50px"
+        showDiv.style.backgroundColor=`black`
+      })
+ }
+
+
